fbea6b80ce06f5a4907d465d9dfd7522ba467ceb
wvtpaf <wvtpaf@clrmail.com>
2014-12-10 14:42:51 +0100

    Update example.ml
    Filtering added

--- example.ml
+++ example.ml
@@ -3,4 +3,14 @@ let factors n =
       if n = 1 then [] else
         if n mod d = 0 then d :: aux d (n / d) else aux (d+1) n
     in
-    aux 2 n
+    let rec find e = function
+        | [] -> false
+        | h::t -> h = e || find e t
+    in
+    let rec cut_redundant l1 l2 =
+        match l1 with
+        | [] -> l2
+        | h::t -> if find h l2 then cut_redundant t l2
+                  else cut_redundant t (h::l2)
+    in
+    List.rev (cut_redundant (aux 2 n) [])
